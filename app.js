e8951f8bd0e97d64a33b1d61ce56b8a09f289168
vluciano8 <vluciano88@gmail.com>
2018-03-18 01:59:33 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,3 +1,39 @@
+    // game values
+
+let min = 1, 
+    max = 10,
+    winningNum = getRandomNum(min, max),
+    guessesLeft = 3;
+    // ui elements
+    const game = document.querySelector('#game'),
+        minNum = document.querySelector('.min-num'),
+        maxNum = document.querySelector('.max-num'),
+        guessBtn = document.querySelector('#guess-btn'),
+        guessInput = document.querySelector('#guess-input'),
+        message = document.querySelector('.message');
+    // assign ui min and max
+minNum.textContent = min;
+maxNum.textContent = max;
+    // play again event listener
+    game.addEventListener('mousedown', function(e){
+        if(e.target.className === 'play-again'){
+            window.location.reload();
+        };
+    });
+
+    // listen for guess
+guessBtn.addEventListener('click', function() {
+    let guess = parseInt(guessInput.value);
+    // validate
+if(isNaN(guess) || guess < min || guess > max){
+    setMessage(`Please enter a number between ${min} and ${max}`, 'red');
+};
+    // check if won
+if(guess === winningNum){
+    gameOver(true, `${winningNum} is correct, YOU WIN!`, 'green');
+
+}else{
+    //  wrong number
 guessesLeft = guessesLeft - 1;
     if(guessesLeft === 0 ){
     // game over lost
